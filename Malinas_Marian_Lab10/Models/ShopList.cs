248903827a548ae9805a0cdc4db0e10504dbb2e6
MARIAN-IOAN MĂLINAȘ <marian.malinas@stud.ubbcluj.ro>
2021-12-13 12:39:45 +0200

    Add project files.

---
+++ Malinas_Marian_Lab10/Models/ShopList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+
+namespace Malinas_Marian_Lab10.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
